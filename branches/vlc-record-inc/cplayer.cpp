cdcbdc5515ec0589ae87e122505ec31d62a46d39
Olenka.Joerg <Olenka.Joerg@4efd2022-0513-11df-958a-03df8639dc2f>
2010-02-24 15:52:55 +0000

    - working on player class

--- branches/vlc-record-inc/cplayer.cpp
+++ branches/vlc-record-inc/cplayer.cpp
@@ -26,34 +26,14 @@ CPlayer::CPlayer(QWidget *parent) : QWidget(parent), ui(new Ui::CPlayer)
 {
    ui->setupUi(this);
 
-   QString sPlugIns = QString("--plugin-path=\"%1/plugins\"").arg(QApplication::applicationDirPath());
+   // nothing playing so far ...
+   bIsPlaying   = false;
+   pMedia       = NULL;
+   pMediaPlayer = NULL;
+   pVlcInstance = NULL;
 
-   // preparation of the vlc command
-   const char * const vlc_args[] = {
-      "-I", "dummy",         /* Don't use any interface    */
-      "--ignore-config",     /* Don't use VLC's config     */
-      "--extraintf=logger",  /* log anything               */
-      "--verbose=2",         /* more verbose for debugging */
-      sPlugIns.toAscii().constData()};
-
-   bIsPlaying = false;
-
-   //Initialize an instance of vlc
-   //a structure for the exception is neede for this initalization
-   libvlc_exception_init(&_vlcexcep);
-
-   //create a new libvlc instance
-   _vlcinstance = libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]), vlc_args,&_vlcexcep);  //tricky calculation of the char space used
-   raise (&_vlcexcep);
-
-   // Create a media player playing environement
-   _mp = libvlc_media_player_new (_vlcinstance, &_vlcexcep);
-   raise (&_vlcexcep);
-
-   // connect the two sliders to the corresponding slots (uses Qt's signal / slots technology)
-   // connect(&tPoller, SIGNAL(timeout()), this, SLOT(updateInterface()));
-
-   // tPoller.start(100); //start timer to trigger every 100 ms the updateInterface slot
+   // init exception structure ...
+   libvlc_exception_init(&vlcExcpt);
 }
 
 /* -----------------------------------------------------------------\
@@ -68,16 +48,266 @@ CPlayer::CPlayer(QWidget *parent) : QWidget(parent), ui(new Ui::CPlayer)
 \----------------------------------------------------------------- */
 CPlayer::~CPlayer()
 {
-   /* Stop playing */
-   libvlc_media_player_stop (_mp, &_vlcexcep);
-
-   /* Free the media_player */
-   libvlc_media_player_release (_mp);
-
-   libvlc_release (_vlcinstance);
-   raise (&_vlcexcep);
-
+   releasePlayer();
    delete ui;
+}
+
+/* -----------------------------------------------------------------\
+|  Method: releasePlayer
+|  Begin: 24.02.2010 / 12:25:51
+|  Author: Jo2003
+|  Description: release all the vlc player stuff
+|
+|  Parameters: --
+|
+|  Returns: --
+\----------------------------------------------------------------- */
+void CPlayer::releasePlayer()
+{
+   // release player ...
+   if (pMediaPlayer)
+   {
+      libvlc_media_player_stop (pMediaPlayer, &vlcExcpt);
+      libvlc_media_player_release(pMediaPlayer);
+      pMediaPlayer = NULL;
+   }
+
+   // release media ...
+   if (pMedia)
+   {
+      libvlc_media_release(pMedia);
+      pMedia       = NULL;
+   }
+
+   // release vlc instance ...
+   if (pVlcInstance)
+   {
+      libvlc_release (pVlcInstance);
+      pVlcInstance = NULL;
+   }
+
+   bIsPlaying = false;
+}
+
+/* -----------------------------------------------------------------\
+|  Method: createArgs
+|  Begin: 24.02.2010 / 14:25:51
+|  Author: Jo2003
+|  Description: create argument array, free it with freeArgs
+|
+|  Parameters: ref. to argument list, ref to vlcArgs struct
+|
+|  Returns: number of args
+|
+\----------------------------------------------------------------- */
+int CPlayer::createArgs (const QStringList &lArgs, Ui::vlcArgs &args)
+{
+   int i         = 0;
+   args.argArray = new char *[lArgs.count()];
+
+   if (args.argArray)
+   {
+      args.argc = lArgs.count();
+
+      for (i = 0; i < args.argc; i++)
+      {
+         args.argArray[i] = new char[lArgs[i].size() + 1];
+
+         if (args.argArray[i])
+         {
+            strcpy (args.argArray[i], lArgs[i].toAscii().constData());
+         }
+      }
+   }
+
+   return i;
+}
+
+/* -----------------------------------------------------------------\
+|  Method: freeArgs
+|  Begin: 24.02.2010 / 15:25:51
+|  Author: Jo2003
+|  Description: free argument array
+|
+|  Parameters: ref. to vlcArgs struct
+|
+|  Returns: --
+\----------------------------------------------------------------- */
+void CPlayer::freeArgs (Ui::vlcArgs &args)
+{
+   for (int i = 0; i < args.argc; i++)
+   {
+      delete [] args.argArray[i];
+   }
+
+   delete [] args.argArray;
+   args.argArray = NULL;
+   args.argc     = 0;
+}
+
+/* -----------------------------------------------------------------\
+|  Method: initPlayer
+|  Begin: 24.02.2010 / 14:00:51
+|  Author: Jo2003
+|  Description: init player with arguments
+|
+|  Parameters: list of arguments
+|
+|  Returns: --
+\----------------------------------------------------------------- */
+void CPlayer::initPlayer(QStringList &slArgs)
+{
+   Ui::vlcArgs args;
+
+   releasePlayer();
+
+   // reset exception stuff ...
+   libvlc_exception_init(&vlcExcpt);
+
+   if (slArgs.size() == 0)
+   {
+      // no arguments given ... create standard arguments ...
+      slArgs << "-I";
+      slArgs << "dummy";
+      slArgs << "--ignore-config";
+#ifndef QT_NO_DEBUG
+      slArgs << "--extraintf=logger";
+      slArgs << "--verbose=2";
+#endif
+   }
+
+   // add plugin path ...
+   slArgs << QString("--plugin-path=\"%1/plugins\"")
+              .arg(QApplication::applicationDirPath());
+
+   // fill vlcArgs struct ...
+   createArgs(slArgs, args);
+
+   // arguments there ... ?
+   if (args.argArray)
+   {
+      //create a new libvlc instance
+      pVlcInstance = libvlc_new(args.argc, args.argArray, &vlcExcpt);
+      raise (&vlcExcpt);
+
+      // Create a media player playing environement
+      pMediaPlayer = libvlc_media_player_new (pVlcInstance, &vlcExcpt);
+      raise (&vlcExcpt);
+
+      // add player to window ...
+      connect_to_wnd(pMediaPlayer, ui->fVideo->winId(), &vlcExcpt);
+
+      raise(&vlcExcpt);
+
+      freeArgs(args);
+   }
+}
+
+/* -----------------------------------------------------------------\
+|  Method: setMedia
+|  Begin: 24.02.2010 / 16:00:51
+|  Author: Jo2003
+|  Description: set media
+|
+|  Parameters: media MRL
+|
+|  Returns: --
+\----------------------------------------------------------------- */
+void CPlayer::setMedia(const QString &sMrl)
+{
+   // is player playing ... ?
+   libvlc_media_t *curMedia = libvlc_media_player_get_media (pMediaPlayer, &vlcExcpt);
+
+   // if playing, stop and then release media ...
+   if (curMedia)
+   {
+      if (bIsPlaying)
+      {
+         libvlc_media_player_stop(pMediaPlayer, &vlcExcpt);
+      }
+
+      // release media ...
+      libvlc_media_release (curMedia);
+      pMedia = NULL;
+   }
+
+   // reset exception stuff ...
+   libvlc_exception_init(&vlcExcpt);
+
+   // create new media ...
+   pMedia = libvlc_media_new (pVlcInstance, sMrl.toAscii().constData(), &vlcExcpt);
+   raise(&vlcExcpt);
+
+   // add media ...
+   libvlc_media_player_set_media (pMediaPlayer, pMedia, &vlcExcpt);
+   raise(&vlcExcpt);
+}
+
+/* -----------------------------------------------------------------\
+|  Method: play
+|  Begin: 24.02.2010 / 16:00:51
+|  Author: Jo2003
+|  Description: play media
+|
+|  Parameters: --
+|
+|  Returns: --
+\----------------------------------------------------------------- */
+void CPlayer::play()
+{
+   // reset exception stuff ...
+   libvlc_exception_init(&vlcExcpt);
+
+   libvlc_media_player_play (pMediaPlayer, &vlcExcpt);
+
+   if (!raise(&vlcExcpt))
+   {
+      bIsPlaying = true;
+   }
+}
+
+/* -----------------------------------------------------------------\
+|  Method: stop
+|  Begin: 24.02.2010 / 16:00:51
+|  Author: Jo2003
+|  Description: stop playing
+|
+|  Parameters: --
+|
+|  Returns: --
+\----------------------------------------------------------------- */
+void CPlayer::stop()
+{
+   if (bIsPlaying)
+   {
+      libvlc_media_player_stop (pMediaPlayer, &vlcExcpt);
+      bIsPlaying = false;
+   }
+}
+
+/* -----------------------------------------------------------------\
+|  Method: pause
+|  Begin: 24.02.2010 / 16:00:51
+|  Author: Jo2003
+|  Description: pause / unpause playing
+|
+|  Parameters: --
+|
+|  Returns: --
+\----------------------------------------------------------------- */
+void CPlayer::pause()
+{
+   // reset exception stuff ...
+   libvlc_exception_init(&vlcExcpt);
+
+   if (bIsPlaying && libvlc_media_player_can_pause(pMediaPlayer, &vlcExcpt))
+   {
+      if (!raise(&vlcExcpt))
+      {
+         libvlc_media_player_pause(pMediaPlayer, &vlcExcpt);
+         raise(&vlcExcpt);
+      }
+   }
 }
 
 /* -----------------------------------------------------------------\
@@ -103,87 +333,6 @@ void CPlayer::changeEvent(QEvent *e)
 }
 
 /* -----------------------------------------------------------------\
-|  Method: playFile
-|  Begin: 24.02.2010 / 11:46:10
-|  Author: Jo2003
-|  Description: play given file in widget
-|
-|  Parameters: file name
-|
-|  Returns: --
-\----------------------------------------------------------------- */
-void CPlayer::playFile(QString file)
-{
-   //the file has to be in one of the following formats /perhaps a little bit outdated)
-   /*
-    [file://]filename              Plain media file
-    http://ip:port/file            HTTP URL
-    ftp://ip:port/file             FTP URL
-    mms://ip:port/file             MMS URL
-    screen://                      Screen capture
-    [dvd://][device][@raw_device]  DVD device
-    [vcd://][device]               VCD device
-    [cdda://][device]              Audio CD device
-    udp:[[<source address>]@[<bind address>][:<bind port>]]
-    */
-
-   /* Create a new LibVLC media descriptor */
-   _m = libvlc_media_new (_vlcinstance, file.toAscii(), &_vlcexcep);
-   raise(&_vlcexcep);
-
-   libvlc_media_player_set_media (_mp, _m, &_vlcexcep);
-   raise(&_vlcexcep);
-
-   // Please note!
-   //
-   // passing the widget to the lib shows vlc at which position it should show up
-   // vlc automatically resizes the video to the given size of the widget
-   // and it even resizes it, if the size changes at the playing
-
-   /* Get our media instance to use our window */
-#if defined(Q_OS_WIN)
-   libvlc_media_player_set_hwnd(_mp, ui->fVideo->winId(), &_vlcexcep ); // for vlc 1.0
-#elif defined(Q_OS_MAC)
-   libvlc_media_player_set_agl (_mp, ui->fVideo->winId(), &_vlcexcep); // for vlc 1.0
-#else //Linux
-   libvlc_media_player_set_xwindow(_mp, ui->fVideo->winId(), &_vlcexcep ); // for vlc 1.0
-#endif
-   raise(&_vlcexcep);
-
-   /* Play */
-   libvlc_media_player_play (_mp, &_vlcexcep );
-   raise(&_vlcexcep);
-
-   bIsPlaying = true;
-}
-
-/*
-void CPlayer::updateInterface()
-{
-   if(!bIsPlaying)
-   {
-      return;
-   }
-
-   // It's possible that the vlc doesn't play anything
-   // so check before
-   libvlc_media_t *curMedia = libvlc_media_player_get_media (_mp, &_vlcexcep);
-   libvlc_exception_clear(&_vlcexcep);
-
-   if (!curMedia)
-   {
-      return;
-   }
-
-   float pos=libvlc_media_player_get_position (_mp, &_vlcexcep);
-   int siderPos=(int)(pos*(float)(POSITION_RESOLUTION));
-   _positionSlider->setValue(siderPos);
-   int volume=libvlc_audio_get_volume (_vlcinstance,&_vlcexcep);
-   _volumeSlider->setValue(volume);
-}
-*/
-
-/* -----------------------------------------------------------------\
 |  Method: raise
 |  Begin: 24.02.2010 / 11:46:10
 |  Author: Jo2003
@@ -191,16 +340,20 @@ void CPlayer::updateInterface()
 |               error message
 |  Parameters: pointer to exception
 |
-|  Returns: --
+|  Returns: 0 ==> ok
+|        else ==> any error
 \----------------------------------------------------------------- */
-void CPlayer::raise(libvlc_exception_t * ex)
+int CPlayer::raise(libvlc_exception_t * ex)
 {
-   if (libvlc_exception_raised (ex))
+   int iRV = libvlc_exception_raised (ex);
+   if (iRV)
    {
       QMessageBox::critical(this, tr("LibVLC Error!"),
                             tr("LibVLC reports following error:\n%1")
                             .arg(libvlc_exception_get_message(ex)));
    }
+
+   return iRV;
 }
 
 /************************* History ***************************\
