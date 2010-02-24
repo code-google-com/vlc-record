afa1dad2297bf35faca9cafdcaf0c59f248c6dc4
Olenka.Joerg <Olenka.Joerg@4efd2022-0513-11df-958a-03df8639dc2f>
2010-02-24 15:52:55 +0000

    - working on player class

--- cplayer.h
+++ cplayer.h
@@ -21,9 +21,26 @@
 //===================================================================
 // namespace
 //===================================================================
-namespace Ui {
-    class CPlayer;
+namespace Ui
+{
+   class CPlayer;
+   typedef struct _vlcArgs
+   {
+      char** argArray;
+      int    argc;
+   } vlcArgs;
 }
+
+//===================================================================
+// macro to connect player to hardware ...
+//===================================================================
+#ifdef Q_OS_WIN        // on windows ...
+   #define connect_to_wnd(a, b, c) libvlc_media_player_set_hwnd (a, b, c)
+#elif defined Q_OS_MAC // on MAC OS
+   #define connect_to_wnd(a, b, c) libvlc_media_player_set_agl (a, b, c)
+#else                  // on Linux
+   #define connect_to_wnd(a, b, c) libvlc_media_player_set_xwindow (a, b, c)
+#endif
 
 /********************************************************************\
 |  Class: CPlayer
@@ -39,19 +56,26 @@ class CPlayer : public QWidget
 public:
    CPlayer(QWidget *parent = 0);
    ~CPlayer();
-   void playFile(QString file);
+   void initPlayer (QStringList &slArgs);
+   void setMedia (const QString &sMrl);
+   void play();
+   void stop();
+   void pause();
 
 protected:
    void changeEvent(QEvent *e);
-   void raise(libvlc_exception_t * ex);
+   int  raise(libvlc_exception_t * ex);
+   void releasePlayer ();
+   int  createArgs (const QStringList &lArgs, Ui::vlcArgs& args);
+   void freeArgs (Ui::vlcArgs& args);
 
 private:
-   Ui::CPlayer *ui;
-   bool   bIsPlaying;
-   libvlc_exception_t _vlcexcep;
-   libvlc_instance_t *_vlcinstance;
-   libvlc_media_player_t *_mp;
-   libvlc_media_t *_m;
+   Ui::CPlayer           *ui;
+   bool                   bIsPlaying;
+   libvlc_exception_t     vlcExcpt;
+   libvlc_instance_t     *pVlcInstance;
+   libvlc_media_player_t *pMediaPlayer;
+   libvlc_media_t        *pMedia;
 };
 
 #endif /* __022410__CPLAYER_H */
