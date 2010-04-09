ab1ed3b4deb3aa9466aa21e0e58a6e005603414c
Olenka.Joerg <Olenka.Joerg@4efd2022-0513-11df-958a-03df8639dc2f>
2010-04-09 14:04:00 +0000

    - tag version x.28

---
+++ x.28/cepgbrowser.h
@@ -0,0 +1,61 @@
+/*********************** Information *************************\
+| $HeadURL$
+|
+| Author: Jo2003
+|
+| Begin: 18.01.2010 / 16:05:56
+|
+| Last edited by: $Author$
+|
+| $Id$
+\*************************************************************/
+#ifndef __011810__CEPGBROWSER_H
+   #define __011810__CEPGBROWSER_H
+
+#include <QTextBrowser>
+#include <QMap>
+#include "ckartinaxmlparser.h"
+#include "templates.h"
+
+#include "clogfile.h"
+#include "defdef.h"
+
+/********************************************************************\
+|  Class: CEpgBrowser
+|  Date:  18.01.2010 / 16:06:32
+|  Author: Jo2003
+|  Description: textbrowser with epg functionality
+|
+\********************************************************************/
+class CEpgBrowser : public QTextBrowser
+{
+   Q_OBJECT
+
+public:
+    CEpgBrowser(QWidget *parent = 0);
+    void DisplayEpg(QVector<cparser::SEpg> epglist,
+                    const QString &sName, int iChanID,
+                    uint uiGmt, bool bHasArchiv);
+
+    void SetTimeShift (int iTs) { iTimeShift = iTs; }
+    int  GetCid () { return iCid; }
+    QString ShowName (uint uiTimeT);
+    void EnlargeFont ();
+    void ReduceFont ();
+    void ChangeFontSize (int iSz);
+
+protected:
+    bool NowRunning (const QDateTime &startThis, const QDateTime &startNext = QDateTime());
+    bool ArchivAvailable (uint uiThisShow);
+
+private:
+    int                 iTimeShift;
+    int                 iCid;
+    QMap<uint, QString> mProgram;
+};
+
+#endif /* __011810__CEPGBROWSER_H */
+/************************* History ***************************\
+| $Log$
+\*************************************************************/
+
