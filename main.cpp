be5e1bc4c028eeae59d8c566ae7f517da1f153ca
Olenka.Joerg <Olenka.Joerg@4efd2022-0513-11df-958a-03df8639dc2f>
2010-07-13 14:06:07 +0000

    - backport to use libVLC 1.05 because it's more stable

---
+++ main.cpp
@@ -0,0 +1,84 @@
+/*********************** Information *************************\
+| $HeadURL$
+|
+| Author: Jo2003
+|
+| Begin: 19.01.2010 / 15:57:06
+|
+| Last edited by: $Author$
+|
+| $Id$
+\*************************************************************/
+#include <QtGui/QApplication>
+#include <QTranslator>
+#include "recorder.h"
+#include "cvlcrecdb.h"
+#include "cshowinfo.h"
+
+#ifdef DINCLUDEPLUGS
+#include <QtPlugin>
+Q_IMPORT_PLUGIN(qgif)
+Q_IMPORT_PLUGIN(qico)
+#endif // DINCLUDEPLUGS
+
+// make logging class available everywhere ...
+CLogFile VlcLog;
+
+// make directory names available globally ...
+CDirStuff *pFolders;
+
+// db storage class must be global ...
+CVlcRecDB *pDb;
+
+// make show info global available ...
+CShowInfo showInfo;
+
+/* -----------------------------------------------------------------\
+|  Method: main / program entry
+|  Begin: 19.01.2010 / 15:57:36
+|  Author: Jo2003
+|  Description: program entry point
+|
+|  Parameters: command line parameters
+|
+|  Returns: 0 ==> ok
+|        else ==> any error
+\----------------------------------------------------------------- */
+int main(int argc, char *argv[])
+{
+   int          iRV = -1;
+   QTranslator  trans;
+   QApplication a(argc, argv);
+   a.installTranslator(&trans);
+
+   // create directory stuff ...
+   pFolders = new CDirStuff;
+
+   if (pFolders)
+   {
+      // is folder stuff initialized successfully ...?
+      if (pFolders->isInitialized())
+      {
+         pDb = new CVlcRecDB();
+
+         if (pDb)
+         {
+            Recorder w(&trans);
+            w.show();
+            iRV = a.exec();
+
+            // free / close database ...
+            delete pDb;
+         }
+      }
+
+      // free mem ...
+      delete pFolders;
+   }
+
+   return iRV;
+}
+
+/************************* History ***************************\
+| $Log$
+\*************************************************************/
