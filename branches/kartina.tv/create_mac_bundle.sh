f09a93c9072852d5fcb209f8194184bf306ddd6f
Olenka.Joerg@gmail.com <Olenka.Joerg@gmail.com@4efd2022-0513-11df-958a-03df8639dc2f>
2011-12-22 20:29:30 +0000

    - first changes for new Mac version

--- branches/kartina.tv/create_mac_bundle.sh
+++ branches/kartina.tv/create_mac_bundle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TMPFILE=/tmp/plist.tmp
-CONTENTS=vlc-record.app/Contents
+CONTENTS=kartina_tv.app/Contents
 
 MINORVER=`cat version.mak | awk -F '=' '{if($1 == "MINORVER"){print $2}}'`
 MAJORVER=`cat version.mak | awk -F '=' '{if($1 == "CLMAJORVER"){print $2}}'`
@@ -11,10 +11,10 @@ mkdir -p $CONTENTS/PlugIns/modules
 cp ../*.qm $CONTENTS/Resources/language/
 cp ../modules/*.mod $CONTENTS/PlugIns/modules/
 rm -f $CONTENTS/PlugIns/modules/*libvlc*
-cp ../resources/vlc-record.icns $CONTENTS/Resources/
-sed -e 's/vlc-record.rc/vlc-record.icns/g' -e 's/yourcompany/Jo2003/g' $CONTENTS/Info.plist >$TMPFILE
+cp ../resources/kartina.icns $CONTENTS/Resources/kartina_tv.icns
+sed -e 's/kartina_tv.rc/kartina_tv.icns/g' -e 's/yourcompany/Jo2003/g' $CONTENTS/Info.plist >$TMPFILE
 iconv -f ASCII -t UTF-8 $TMPFILE >$CONTENTS/Info.plist
-macdeployqt vlc-record.app -dmg
-mv vlc-record.dmg vlc-record-${MAJORVER}.${MINORVER}-${DATESTR}-mac.dmg
+macdeployqt kartina_tv.app -dmg
+mv kartina_tv.dmg kartina_tv-${MAJORVER}.${MINORVER}-${DATESTR}-mac.dmg
 cd ..
 
