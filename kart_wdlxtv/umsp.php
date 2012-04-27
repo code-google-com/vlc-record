70ca86ab9f68e62af8095224777caead1176a007
Olenka.Joerg <Olenka.Joerg@4efd2022-0513-11df-958a-03df8639dc2f>
2012-04-27 05:42:32 +0000

    a try with stacked layout

--- kart_wdlxtv/umsp.php
+++
@@ -1,9 +0,0 @@
-<?php
-$myMediaItems[] = array(
-   'id'            => 'umsp://plugins/kartina',
-   'parentID'      => '0',
-   'dc:title'      => 'Картина.тв',
-   'upnp:class'    => 'object.container',
-   'upnp:album_art'=> 'http://127.0.0.1/umsp/plugins/kartina/images/logo1.jpg'
-);
-?>
