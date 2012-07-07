c84159742bc7410b3963f55df89da335cdca9710
rovic2735 <rovic2735@4efd2022-0513-11df-958a-03df8639dc2f>
2012-07-07 00:37:44 +0000

    - updates up to r764

--- trunk/ktv-recorder/cparentalcontroldlg.cpp
+++ trunk/ktv-recorder/cparentalcontroldlg.cpp
@@ -299,11 +299,15 @@ void CParentalControlDlg::slotBuildVodManager(const QString &str)
     QRadioButton *pRadHide, *pRadShow, *pRadPass;
     QString       sLabel;
     QFont         font;
-    QStringList   sl;
-    const char   *pGenre;
+    QMap<QString, QString> transMap;
 
     // help translate the vod manager strings ...
-    sl << tr("blood") << tr("violence") << tr("obscene") << tr("porn") << tr("horror");
+    transMap.insert("blood",    tr("blood"));
+    transMap.insert("violence", tr("violence"));
+    transMap.insert("obscene",  tr("obscene"));
+    transMap.insert("obsence",  tr("obscene")); // fix a small typo in API ...
+    transMap.insert("porn",     tr("porn"));
+    transMap.insert("horror",   tr("horror"));
 
     // clear layout ...
     if (pLayout)
@@ -324,11 +328,8 @@ void CParentalControlDlg::slotBuildVodManager(const QString &str)
        // make forms for every rate ...
        for (int i = 0; i < vodRatesVector.count(); i++)
        {
-          // fix a small typo in API ...
-          pGenre    = (vodRatesVector[i].sGenre == "obsence") ? "obscene" : vodRatesVector[i].sGenre.toUtf8().constData();
-
-          // translate label ...
-          sLabel    = tr(pGenre);
+           // translate label ...
+           sLabel    = transMap.value(vodRatesVector[i].sGenre, vodRatesVector[i].sGenre);
 
           // make label better looking ...
           sLabel[0] = sLabel[0].toUpper();
@@ -392,7 +393,7 @@ void CParentalControlDlg::slotNewPCodeSet()
    // internally store changed pcode ...
    sTempPasswd = m_ui->lineNewPCode->text();
 
-   // save to database ...
+   // save to database the new ErosPasswd . . .
    pDb->setPassword("ErosPasswdEnc", sTempPasswd);
 
    slotEnablePCodeForm();
