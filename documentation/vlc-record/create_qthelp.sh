17152dec77c90e2c56396150a094c7d534c111e1
Olenka.Joerg <Olenka.Joerg@4efd2022-0513-11df-958a-03df8639dc2f>
2012-07-16 09:33:29 +0000

    - prepare help branding

--- documentation/vlc-record/create_qthelp.sh
+++ documentation/vlc-record/create_qthelp.sh
@@ -33,11 +33,13 @@ case $1 in
    
 esac
 
+# delete qch and qhc files ...
+rm -f *.qch *.qhc
+
 for i in $LANGUAGES ; do
    # patch files ...
    sed -e "s/\[%PROGRAM%\]/$PROGRAM/g" -e "s/\[%SERVICE%\]/$SERVICE/g" -e "s/\[%APISERVER%\]/$APISERVER/g" help_$i.tmpl > help_$i.html
    sed -e "s/\[%PROGRAM%\]/$PROGRAM/g" help_$i.qhp_tmpl > help_$i.qhp
-   
    
    if [ $HIDDENFEATURES -gt 0 ]; then
       sed -e "s/^.*\[%HIDDEN%\].*$//g" help_$i.html > $TMPFILE
