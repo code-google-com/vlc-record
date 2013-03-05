62e9d066cdb86eaebb31bad6313d3e93d8e598d4
Olenka.Joerg <Olenka.Joerg@4efd2022-0513-11df-958a-03df8639dc2f>
2013-03-05 09:25:21 +0000

    - cosmetic changes

--- trunk/webend/file_index.php
+++ trunk/webend/file_index.php
@@ -251,16 +251,14 @@
     echo "      <h2>Changelog:</h2>\n"
         ."      <pre>\n"
         .htmlspecialchars(file_get_contents('./changelog.log'))
-        ."      </pre>";
+        ."      </pre>\n";
   }
 
 ?>
       <p>
-         <a href="http://validator.w3.org/check?uri=referer"><img
-            src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
-         <a href="http://jigsaw.w3.org/css-validator/check/referer"><img
-            src="http://jigsaw.w3.org/css-validator/images/vcss" alt="CSS ist valide!" height="31" width="88" /></a>
-     </p>
+         <a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+         <a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="CSS ist valide!" height="31" width="88" /></a>
+      </p>
    </div>
 </body>
 </html>
