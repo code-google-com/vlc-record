c5f06b47ef374711c87680784b026354acc703b0
Olenka.Joerg <Olenka.Joerg@4efd2022-0513-11df-958a-03df8639dc2f>
2013-07-19 07:09:21 +0000

    - make sure to don't hide information for native clients

--- documentation/vlc-record/create_qthelp.sh
+++ documentation/vlc-record/create_qthelp.sh
@@ -56,16 +56,19 @@ for CUST in $CUSTS ; do
     rm -f *.qch *.qhc
 
     for i in $LANGUAGES ; do
-       # patch files ...
-       sed -e "s/\[%PROGRAM%\]/$PROGRAM/g" -e "s/\[%SERVICE%\]/$SERVICE/g" -e "s/\[%APISERVER%\]/$APISERVER/g" help_$i.tmpl > help_$i.html
-       sed -e "s/\[%PROGRAM%\]/$PROGRAM/g" help_$i.qhp_tmpl > help_$i.qhp
+        # patch files ...
+        sed -e "s/\[%PROGRAM%\]/$PROGRAM/g" -e "s/\[%SERVICE%\]/$SERVICE/g" -e "s/\[%APISERVER%\]/$APISERVER/g" help_$i.tmpl > help_$i.html
+        sed -e "s/\[%PROGRAM%\]/$PROGRAM/g" help_$i.qhp_tmpl > help_$i.qhp
 
-       if [ $HIDDENFEATURES -gt 0 ]; then
-          sed -e "s/^.*\[%HIDDEN%\].*$//g" help_$i.html > $TMPFILE
-          mv $TMPFILE help_$i.html
-          sed -e "s/^.*\[%HIDDEN%\].*$//g" help_$i.qhp > $TMPFILE
-          mv $TMPFILE help_$i.qhp
-       fi
+        if [ $HIDDENFEATURES -gt 0 ]; then
+            sed -e "s/^.*\[%HIDDEN%\].*$//g" help_$i.html > $TMPFILE
+            mv $TMPFILE help_$i.html
+            sed -e "s/^.*\[%HIDDEN%\].*$//g" help_$i.qhp > $TMPFILE
+            mv $TMPFILE help_$i.qhp
+        fi
+       
+        sed -e "s/<!-- \[%HIDEPROJECT%\]//g" -e "s/\[%HIDEPROJECT%\] -->//g" help_$i.html > $TMPFILE
+        mv $TMPFILE help_$i.html
 
         qhelpgenerator help_$i.qhp -o help_$i.qch
         qcollectiongenerator help_$i.qhcp -o help_$i.qhc
