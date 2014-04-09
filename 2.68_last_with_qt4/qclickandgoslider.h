a0947001838040c55abe5f22e8b056fefe3b4d9b
Olenka.Joerg <Olenka.Joerg@4efd2022-0513-11df-958a-03df8639dc2f>
2014-04-09 10:16:16 +0000

    - make a tag for last version for Qt4

---
+++ 2.68_last_with_qt4/qclickandgoslider.h
@@ -0,0 +1,129 @@
+/*------------------------------ Information ---------------------------*//**
+ *
+ *  $HeadURL$
+ *
+ *  @file     qclickandgoslider.h
+ *
+ *  @author   Jo2003, inspired by Ralf Neubersch
+ *
+ *  @date     16.02.2012
+ *
+ *  $Id$
+ *
+ *///------------------------- (c) 2012 by Jo2003  --------------------------
+#ifndef __20120216_QCLICKANDGOSLIDER_H
+   #define __20120216_QCLICKANDGOSLIDER_H
+
+#include <QSlider>
+#include <QMouseEvent>
+
+//---------------------------------------------------------------------------
+//! \class   QClickAndGoSlider
+//! \date    16.02.2012
+//! \author  Ralf Neubersch / Jo2003
+//! \brief   a custom slider with support of click'n'go
+//---------------------------------------------------------------------------
+class QClickAndGoSlider : public QSlider
+{
+   Q_OBJECT
+
+public:
+   QClickAndGoSlider ( QWidget * parent = 0 )
+      : QSlider(parent)
+   {
+      _iHandleRange = 80;
+   }
+
+   QClickAndGoSlider ( Qt::Orientation orientation, QWidget * parent = 0 )
+      : QSlider(orientation, parent)
+   {
+      _iHandleRange = 80;
+   }
+
+   virtual ~QClickAndGoSlider ()
+   {
+      _iHandleRange = 80;
+   }
+
+   //---------------------------------------------------------------------------
+   //
+   //! \brief   modify the range around the slider handle so it doesn't catch
+   //!          click events.
+   //!          std: 80. Larger values will reduce the range, lower will raise
+   //
+   //! \author  Jo2003
+   //! \date    11.11.2013
+   //
+   //! \param   --
+   //
+   //! \return  --
+   //---------------------------------------------------------------------------
+   virtual void setHandleRangeVal(int i)
+   {
+      _iHandleRange = i;
+   }
+
+protected:
+
+   //---------------------------------------------------------------------------
+   //
+   //! \brief   override QWidget::mousePressEvent()
+   //
+   //! \author  Ralf Neubersch / Jo2003
+   //! \date    16.02.2012
+   //
+   //! \param   event pointer to mouse event
+   //
+   //! \return  --
+   //---------------------------------------------------------------------------
+   virtual void mousePressEvent (QMouseEvent * event)
+   {
+      // left click ...
+      if (event->button() == Qt::LeftButton)
+      {
+         int pos;
+         int range = maximum() - minimum();
+
+         if (orientation() == Qt::Vertical)
+         {
+            pos = minimum() + (range * (height() - event->y())) / height();
+         }
+         else
+         {
+            pos = minimum() + (range * event->x()) / width();
+         }
+
+         // make sure pos is in between minimum and maximum ...
+         pos = (pos < minimum()) ? minimum() : ((pos > maximum()) ? maximum() : pos);
+
+         // check if position is different from slider ...
+         /// Note: We must use a practical threshold value here.
+         /// So we use the range which should be handled by
+         /// the slider / _iHandleRange so we must not click at the 100%
+         /// right position to get the "old" normal
+         /// slider behavior.
+         if (abs(value() - pos) > (range / _iHandleRange))
+         {
+            setValue(pos);
+            emit sigClickNGo(pos);
+            event->accept();
+
+            // no further handling needed ...
+            return;
+         }
+      }
+
+      // not handled --> delegate ...
+      event->ignore();
+      QSlider::mousePressEvent(event);
+   }
+
+private:
+   int _iHandleRange;
+
+signals:
+   // special signal so we know for sure how to handle...
+   void sigClickNGo(int newPos);
+};
+
+#endif // __20120216_QCLICKANDGOSLIDER_H
---
+++ 2.68_last_with_qt4/qoverlayedcontrol.h
@@ -0,0 +1,70 @@
+/*------------------------------ Information ---------------------------*//**
+ *
+ *  $HeadURL$
+ *
+ *  @file     qoverlayedcontrol.h
+ *
+ *  @author   Jo2003
+ *
+ *  @date     09.11.2012
+ *
+ *  $Id$
+ *
+ *///------------------------- (c) 2012 by Jo2003  --------------------------
+#ifndef __20121109_QOVERLAYEDCONTROL_H
+   #define __20121109_QOVERLAYEDCONTROL_H
+
+#include <QEvent>
+#include <QShowEvent>
+#include <QMouseEvent>
+#include <QPoint>
+#include "qfadewidget.h"
+
+namespace Ui {
+  class QOverlayedControl;
+}
+
+//---------------------------------------------------------------------------
+//! \class   QOverlayedControl
+//! \date    09.11.2012
+//! \author  Jo2003
+//! \brief   a player control widget to show while player is fullscreen
+//---------------------------------------------------------------------------
+class QOverlayedControl : public QFadeWidget
+{
+  Q_OBJECT
+
+public:
+  QOverlayedControl(QWidget *parent = 0, Qt::WindowFlags f = 0);
+  virtual ~QOverlayedControl();
+  void chgWindowed (bool on);
+  void chgFullscreen (bool on);
+
+protected:
+  virtual void changeEvent(QEvent *e);
+  virtual void enterEvent(QEvent *e);
+  virtual void leaveEvent(QEvent *e);
+  virtual void mousePressEvent(QMouseEvent *e);
+  virtual void mouseMoveEvent(QMouseEvent *e);
+  virtual void mouseReleaseEvent(QMouseEvent *e);
+  virtual void wheelEvent(QWheelEvent *e);
+
+signals:
+  void sigMouseAboveOverlay ();
+  void sigMouseLeavesOverlay ();
+  void wheel(bool w);
+
+private slots:
+  void slotMouseEntersMoveHandle ();
+  void slotMouseLeavesMoveHandle ();
+  void on_pushHide_clicked();
+
+public slots:
+
+private:
+  Ui::QOverlayedControl *ui;
+  QPoint _offset;
+  bool   _mouseOverMoveHandle;
+};
+
+#endif // __20121109_QOVERLAYEDCONTROL_H
